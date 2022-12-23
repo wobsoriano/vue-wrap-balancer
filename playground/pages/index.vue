938ffb97cadf360afb08070cbe6706b758ef8db2
wobsoriano <sorianorobertc@gmail.com>
2022-12-22 16:32:11 -0800

    add playground

---
+++ playground/pages/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import Balancer from 'vue-wrap-balancer'
+import { useSpring } from 'vue-use-spring'
+
+const ratio = useSpring({ w: 0.65 })
+
+function handleRangeChange(payload: Event) {
+  ratio.w = Number((payload.target as any).value) / 100
+}
+</script>
+
+<template>
+  <div>
+    <Head>
+      <Link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
+    </Head>
+    <div class="demo-container">
+      <div class="controller">
+        <input
+          type="range"
+          defaultValue="65"
+          @change="handleRangeChange"
+        >
+      </div>
+      <div class="demo" style="width: 480px; max-width: 100%">
+        <div style="text-align: center; position: relative;">
+          <div>
+            <h2 class="ratio-ruler">
+              <Balancer>
+                <span>The quick brown fox jumps over the lazy dog</span>
+              </Balancer>
+            </h2>
+            <h2 class="ratio-title">
+              <Balancer :ratio="ratio.w">
+                The quick brown fox jumps over the lazy dog
+              </Balancer>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,137 @@
-export const one = 1
-export const two = 2
+import { defineComponent, h, ref, watchPostEffect, withDirectives } from 'vue'
+import { nanoid } from 'nanoid'
+import { BindOnceDirective } from 'vue-bind-once'
+// @ts-expect-error: Nuxt
+import { useHead } from '#imports'
+
+const SYMBOL_KEY = '__wrap_balancer'
+
+const relayout = (
+  id: string | number,
+  ratio: number,
+  wrapper?: HTMLElement,
+) => {
+  wrapper
+    = wrapper || (document.querySelector(`[data-br="${id}"]`) as HTMLElement)
+
+  if (!wrapper)
+    return
+
+  const container = wrapper.parentElement as HTMLElement
+
+  const update = (width: number) => (wrapper!.style.maxWidth = `${width}px`)
+
+  // Reset wrapper width
+  wrapper.style.maxWidth = ''
+
+  // Get the intial container size
+  const w = container.clientWidth
+  const h = container.clientHeight
+
+  // Synchronously do binary search and calculate the layout
+  let l = w / 2
+  let r = w
+  let m
+
+  if (w) {
+    while (l + 1 < r) {
+      m = ~~((l + r) / 2)
+      update(m)
+      if (container.clientHeight === h)
+        r = m
+
+      else
+        l = m
+    }
+
+    // Update the wrapper width
+    update(r * ratio + w * (1 - ratio))
+  }
+}
+
+const MINIFIED_RELAYOUT_STR = relayout.toString()
+
+export default defineComponent({
+  props: {
+    /**
+     * The HTML tag to use for the wrapper element.
+     * @default 'span'
+     */
+    as: {
+      type: String,
+      required: false,
+      default: 'span',
+    },
+    /**
+     * The balance ratio of the wrapper width (0 <= ratio <= 1).
+     * 0 means the wrapper width is the same as the container width (no balance, browser default).
+     * 1 means the wrapper width is the minimum (full balance, most compact).
+     * @default 1
+     */
+    ratio: {
+      type: Number,
+      required: false,
+      default: 1,
+    },
+  },
+  setup(props, { slots }) {
+    const As = props.as
+    const id = nanoid(5)
+    const wrapperRef = ref<HTMLElement | null>(null)
+
+    if (typeof useHead !== 'undefined') {
+      useHead({
+        script: [
+          {
+            children: `self.${SYMBOL_KEY}=${MINIFIED_RELAYOUT_STR};self.${SYMBOL_KEY}("${id}",${props.ratio})`,
+          },
+        ],
+      })
+    }
+
+    // Re-balance on content change and on mount/hydration
+    watchPostEffect(() => {
+      if (!wrapperRef.value)
+        return
+      // Re-assign the function here as the component can be dynamically rendered, and script tag won't work in that case.
+      // @ts-expect-error: TODO
+      (self[SYMBOL_KEY] = relayout)(0, props.ratio, wrapperRef.value)
+    })
+
+    watchPostEffect((onInvalidate) => {
+      if (!wrapperRef.value)
+        return
+
+      const container = wrapperRef.value.parentElement as HTMLElement
+      if (!container)
+        return
+
+      const resizeObserver = new ResizeObserver(() => {
+        if (!wrapperRef.value)
+          return
+
+        // Re-assign the function here as the component can be dynamically rendered, and script tag won't work in that case.
+        // @ts-expect-error: TODO
+        self[SYMBOL_KEY](0, props.ratio, wrapperRef.value)
+      })
+
+      resizeObserver.observe(container)
+
+      onInvalidate(() => {
+        resizeObserver.unobserve(container)
+      })
+    })
+
+    return () => withDirectives(h(As, {
+      'data-brr': props.ratio,
+      'ref': wrapperRef,
+      'style': {
+        display: 'inline-block',
+        verticalAlign: 'top',
+        textDecoration: 'inherit',
+      },
+    }, slots.default?.()), [
+      [BindOnceDirective, { 'data-br': id }],
+    ])
+  },
+})
