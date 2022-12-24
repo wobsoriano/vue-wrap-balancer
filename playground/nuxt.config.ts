00b15cfc010e9fd76020edde68c35d99a8979ad8
wobsoriano <sorianorobertc@gmail.com>
2022-12-23 16:30:26 -0800

    try ssr build

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  // ssr: false,
   // modules: ['nuxt-lazy-hydrate'],
   nitro: {
     preset: 'vercel',
