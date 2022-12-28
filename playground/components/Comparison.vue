00a9d220ea4b4e2a8abd183f400e3bdfcff5e3d4
wobsoriano <sorianorobertc@gmail.com>
2022-12-28 10:56:25 -0800

    demo width update

--- playground/components/Comparison.vue
+++ playground/components/Comparison.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useSpring } from 'vue-use-spring'
 
-defineProps({
+const props = defineProps({
   staticWidth: {
     type: Boolean,
     required: false,
@@ -17,6 +17,7 @@ defineProps({
 const api = useSpring({ w: 0.55 })
 
 const slotWidth = computed(() => `calc(${api.w} * var(--w1) + ${150 * (1 - api.w) - 31 * api.w}px)`)
+const demoWidth = computed(() => props.staticWidth ? 'calc(55% + 144px)' : `calc(${api.w * 100}% + ${1 - api.w} * var(--w0))`)
 
 function handleRangeChange(payload: Event) {
   api.w = Number((payload.target as any).value) / 100
@@ -33,7 +34,7 @@ function handleRangeChange(payload: Event) {
       >
     </div>
     <div
-      class="demo" :style="{ width: staticWidth ? 'calc(55% + 144px)' : `calc(${api.w * 100}% + ${1 - api.w} * var(--w0))`, textAlign: align }"
+      class="demo" :style="{ width: demoWidth, textAlign: align }"
     >
       <div>
         <legend>Default</legend>
