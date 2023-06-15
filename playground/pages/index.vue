8b8974f93379f2c1790ce774da0de776141345f7
wobsoriano <sorianorobertc@gmail.com>
2023-06-15 15:23:24 -0700

    update docs

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -58,6 +58,25 @@ import Balancer from 'vue-wrap-balancer'
         <span class="code">1</span> (compact, the default)
       </Balancer>
     </h3>
+    <p style="position: relative;">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="#363636"
+        width="18"
+        style="position: absolute; right: 100%; margin-top: 4; margin-right: 5;"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      Note that if the native CSS balance (<a target="_blank" rel="noopener noreferrer" href="https://developer.chrome.com/blog/css-text-wrap-balance/">
+        text-wrap: balance
+      </a>) is available, Vue Wrap Balancer will use it instead. And the `ratio`
+      option will be ignored in that case.
+    </p>
     <p class="headline">
       <Balancer>How Does It Work?</Balancer>
     </p>
