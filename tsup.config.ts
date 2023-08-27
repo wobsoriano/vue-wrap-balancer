588ab25be6d5a1a255166dbfb74688cc12f23578
wobsoriano <sorianorobertc@gmail.com>
2023-08-26 22:47:15 -0700

    fix: duplicate h identifier

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig((opts) => {
-  return {
-    entry: ['src/index.ts'],
-    format: ['cjs', 'esm'],
-    clean: true,
-    minify: opts.env?.NODE_ENV === 'production',
-    sourcemap: true,
-    dts: true,
-  }
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  clean: true,
+  minify: false,
+  sourcemap: true,
+  dts: true,
 })
